89108ebb5c2234bcc27345444fe2b9186c3f2570
Rishabh Singh <ris8645208@gmail.com>
2022-07-08 12:30:02 +0530

    Add project files.

---
+++ CustomMath.cs
@@ -0,0 +1,35 @@
+﻿namespace CustomMath
+{
+    interface IMathLibInterface
+    {
+        public int AddNumber();
+        public int SubNumber();
+    }
+    class MathLib:IMathLibInterface
+    {
+       readonly int num1, num2;
+
+        public MathLib(int Num1,  int Num2)
+        {
+            num1 = Num1;
+            num2 = Num2;
+        }
+        public int AddNumber ()
+        {
+            return num1 + num2;
+        }
+
+
+        public int SubNumber()
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+            else
+            {
+                return num2 - num1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CustomMath;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathLib mathData = new MathLib(10, 20);
+            Console.WriteLine(Math.Cbrt(19));
+            Console.WriteLine($"Addition is {mathData.AddNumber()}");
+
+            Console.WriteLine($"Subtraction is {mathData.SubNumber()}");
+
+            Console.WriteLine("Enter your Name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your Name is {name} Birth Year is {2022 - age - 1}");
+
+            var inputText = Console.ReadLine();
+            var inputTextStr = Convert.ToString(inputText);
+            File.AppendAllText("Test_create.txt", "\n" + inputText);
+
+            var outputText = File.ReadAllText("Test_create.txt");
+            Console.WriteLine($"The New text is {outputText}");
+
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+        }
+    }
+}
+
